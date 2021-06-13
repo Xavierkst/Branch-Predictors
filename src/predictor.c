92d270be76067ca4e8b9791ae5394693dc0cb247
Yanbing Fang <yanbingfang@utexas.edu>
2021-06-12 23:37:25 -0700

    add gshare

--- src/predictor.c
+++ src/predictor.c
@@ -11,10 +11,13 @@
 //
 // TODO:Student Information
 //
-const char *studentName = "NAME";
-const char *studentID   = "PID";
-const char *email       = "EMAIL";
+const char *studentName1 = "NAME";
+const char *studentID1   = "PID";
+const char *email1       = "EMAIL";
 
+const char *studentName2 = "Yanbing Fang";
+const char *studentID2   = "A59003982";
+const char *email2       = "yafang@ucsd.edu";
 //------------------------------------//
 //      Predictor Configuration       //
 //------------------------------------//
@@ -37,6 +40,10 @@ int verbose;
 //TODO: Add your own Branch Predictor data structures here
 //
 
+uint32_t preTabLength; 
+uint32_t *preTab; // predictor table
+uint32_t gHis; 
+uint32_t result;
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -50,6 +57,26 @@ init_predictor()
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
+
+  switch(bpType){
+    int i = 0;
+
+    case GSHARE:
+      // printf("Start initializing\n");
+      
+      // printf("ghistoryBitsd:%d\n",ghistoryBits);
+      preTabLength = 1 << ghistoryBits;
+
+      preTab = (uint32_t*)malloc( preTabLength*sizeof(uint32_t) );    // predictor table
+      for(i = 0; i < preTabLength; i++){    //initialized to WN
+        preTab[i] = 1;
+      }  
+
+      gHis = 0;  //initialized to NOTTAKEN
+      result = 0;
+  }
+
+
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -68,6 +95,17 @@ make_prediction(uint32_t pc)
     case STATIC:
       return TAKEN;
     case GSHARE:
+      // printf("GSHARE predicting \n");
+      // printf("pc: %d\n",pc);
+
+     result = gHis ^ (pc & (preTabLength - 1));
+      // printf("result: %d, preTabLength%d\n",result,preTabLength);
+      if(preTab[result] > 1){
+        return TAKEN;
+      }else{
+        return NOTTAKEN;
+      }
+
     case TOURNAMENT:
     case CUSTOM:
     default:
@@ -88,4 +126,25 @@ train_predictor(uint32_t pc, uint8_t outcome)
   //
   //TODO: Implement Predictor training
   //
+
+  switch(bpType) {
+    case GSHARE:
+      // printf("GSHARE training\n");
+      // printf("pc: %d\n",pc);
+      
+      gHis <<=  1;
+      if(outcome == TAKEN){
+        gHis |= 1;
+        if(preTab[result] < 3){
+          preTab[result]++;
+        }
+      }else{
+        if(preTab[result]>0){
+          preTab[result]--;
+        }
+      }
+
+      gHis = gHis & (preTabLength - 1);
+  }
+
 }
